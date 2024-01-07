7c4a2662f7b0326012a5938b20122ee931ce1edd
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2024-01-07 14:53:53 +0900

    controllerからview呼び出し

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,31 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-global $head,$style,$body,$end;
-$head='<html><head>';
-$style= <<<EOF
-<style>
-</style>
-EOF;
-$body='<head><body>';
-$end='</body></html>';
-function tag($tag,$txt){
-    return "<{$tag}>".$txt."<{$tag}>";
-}
+
 class HelloController extends Controller
 {
     public function index(){
-        global $head,$style,$body,$end;
-
-        $html=$head . tag('title','Hello/Index').$style.$body
-        .'<a href="/hello/other"> go to Other page</a>'.$end;
-        return $html;
-    }
-    public function other(){
-        global $head,$style,$body,$end;
-
-        $html=$head . tag('title','Hello/Index').$style.$body
-        .'<a href="/hello/other"> go to Other page</a>'.$end;
-        return $html;
+        return view('hello.index');
     }
 }
