3ba9c88af0419e9d54ae9b5381ddac480eb627de
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2024-01-07 15:05:09 +0900

    クエリをそのままviewに表示

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('hello', 'HelloController@index' );
+Route::get('hello/{id?}', 'HelloController@index' );
 Route::get('/', function () {
     return view('welcome');
 });
