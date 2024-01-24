2d9d1fe5c8bae679afcf930b5a4b6337495cc7a4
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2024-01-24 19:44:29 +0900

    p47

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,10 +7,17 @@ function tag($tag,$txt){
     return "<$tag>".$txt."<$tag>";
 
 }
-
+$body='</head><body>';
+$end='</body></html>'
+function tag($tag,$txt){
+    return "<{$tag}>" . $txt."<{$tag}>";
+}
 class HelloController extends Controller
 {
     public function index(Request $request){
+        global $head,$style,$body,$end;
+
+        $html=$head.tag('title','Hello/Index').$style.$body
         $data=[
         'msg'=>'これはコントローラから渡されたメッセージです',
         'id'=>$id
